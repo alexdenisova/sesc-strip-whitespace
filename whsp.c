7d8a0c2947333536676ce06ddf0fcac9080bdffa
Alex Denisova <alexadenisova@gmail.com>
2018-10-05 19:55:02 +0700

    strip whitespace

---
+++ whsp.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[1000], a[1000][1000];
+    int i=0, j=0, k=0,n, l;
+
+    scanf ("%[^\n]s", str);
+    int len = strlen(str);
+    while (sscanf(str, "%s", a[j])!=EOF){
+
+      while(str[i]==' '){
+        i++;
+      }
+      while((str[i]==a[j][k]) && (a[j][k]!='\0')){
+        i++;
+        k++;
+      }
+      for(n=i; n<len; n++){
+        str[n-i]=str[n];
+      }
+      for(n=len; (n-i)<len; n++){
+        str[n-i]='\0';
+      }
+      len=strlen(str);
+      k=0;
+      i=0;
+      j++;
+    }
+
+    for(n=0; n<(j-1); n++){
+    printf("%s ", a[n]);
+    }
+    printf("%s", a[j-1]);
+
+    return 0;
+    
+}
